9e376f47e114dc63687e5a9072ddc07d11f3e929
crg253 <crg253@protonmail.com>
2019-05-08 20:53:25 -0700

    Improved the ScoreBoard class and used it to instantiate a ScoreBoard object, then used that object to keep score.

--- typing_game.py
+++ typing_game.py
@@ -8,17 +8,25 @@ Typing Game gives you three random characters, numbers, or symbols, and asks you
 to type the same thing. User gets one point for each correctly typed set.
 '''
 
-class Score:
-    score = 0
-    start_time = time.time()
-    ave_time = None
-    def update_score(self):
-        self.score += 1
-        self.ave_time = round((time.time() - self.start_time)/self.score, 2)
+class ScoreBoard:
+    def __init__(self):
+        self.total_score = 0
+        self.start_time = time.time()
+    def increase_score(self):
+        self.total_score += 1
+    def calculate_speed(self):
+        return round((time.time() - self.start_time)/self.total_score, 2)
 
-def get_new_score(Score):
-    Score.update_score(Score)
-    return Score.score, Score.ave_time
+def encouraging_message(user_scoreboard:object):
+    print(f"Good Job! Score is {user_scoreboard.total_score}.",\
+        f"Average Speed is {user_scoreboard.calculate_speed()} seconds per line.")
+    return None
+
+def update_score(user_scoreboard:object):
+    user_scoreboard.increase_score()
+    if user_scoreboard.total_score % 3 ==0:
+        encouraging_message(user_scoreboard)
+    return None
 
 def type_line(new_line):
     user_input=''
@@ -40,11 +48,11 @@ def until_quit(game):
 
 @until_quit
 def play_typing_game():
+    my_score_board = ScoreBoard()
     while True:
         new_line = get_new_line()
         type_line(new_line)
-        scores = get_new_score(Score)
-        print(f"Good Job! Score is {scores[0]}. Average Time is {scores[1]}.")
+        update_score(my_score_board)
 
 if __name__ == "__main__":
     play_typing_game()
